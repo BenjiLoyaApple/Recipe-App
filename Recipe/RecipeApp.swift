8713c5214baedd84c1d6a7adc065240bd0ce49e5
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2022-12-14 09:26:40 +0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Benji Loya on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
